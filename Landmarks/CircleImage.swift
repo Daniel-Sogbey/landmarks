48dc8657b7d4e2b3f501b04083919231c758003e
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-05-18 14:22:03 +0000

    creating and combining views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by DaN SoGbEy on 18/05/2023.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle()
+                    .stroke(.white,lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
